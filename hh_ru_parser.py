108ef21fd91a44e96c0722513ffd11fdd074ada2
Artsiom Babrounik <artsiom.babrounik>
2023-03-29 10:18:44 +0400

    Different languages stats parsing.

--- hh_ru_parser.py
+++ hh_ru_parser.py
@@ -12,3 +12,25 @@ response_moskow_30_raw = requests.get(url, params=payload_moskow_30)
 response_moskow_30_raw.raise_for_status()
 response_moskow_30 = response_moskow_30_raw.json()
 print(f"Moskow last 30 days: {response_moskow_30['found']}")
+
+url = "https://api.hh.ru/vacancies"
+
+programming_languages = [
+    "JavaScript",
+    "Java",
+    "Python",
+    "Ruby",
+    "PHP",
+    "C++",
+    "C#",
+    "C",
+    "Go",
+    "Objective-C"
+]
+
+for language in programming_languages:
+    payload = {"area": 1, "text": f"программист {language}", "search_field": "name"}
+    response_raw = requests.get(url, params=payload)
+    response_raw.raise_for_status()
+    response = response_raw.json()
+    print(f"{language}: {response['found']}")
